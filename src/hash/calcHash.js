0121bece330b7d960a47ef0c36a4d98f6b9dd2a5
Maryia Velesiuk <m.velesiuk@andersenlab.com>
2024-01-29 22:38:15 +0200

    feat: add calculateHash function

--- src/hash/calcHash.js
+++ src/hash/calcHash.js
@@ -1,5 +1,17 @@
+import { createReadStream } from "node:fs";
+const { createHash } = await import("node:crypto");
+
 const calculateHash = async () => {
-    // Write your code here 
+  const hash = createHash("sha256");
+
+  const stream = createReadStream("src/hash/files/fileToCalculateHashFor.txt");
+  stream.on("readable", () => {
+    const data = stream.read();
+    if (data) hash.update(data);
+    else {
+      console.log(`${hash.digest("hex")}`);
+    }
+  });
 };
 
 await calculateHash();
