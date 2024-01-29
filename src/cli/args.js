f45961b6f000809350edba63024ef49d09618865
Maryia Velesiuk <m.velesiuk@andersenlab.com>
2024-01-29 21:59:50 +0200

    feat: add parseArgs function

--- src/cli/args.js
+++ src/cli/args.js
@@ -1,5 +1,17 @@
+import { argv } from "node:process";
+
 const parseArgs = () => {
-    // Write your code here 
+  const commandLineArgs = argv.slice(2);
+  const commandLineArgsWithValues = [];
+  for (let i = 0; i < commandLineArgs.length; i++) {
+    console.log(i);
+    if (commandLineArgs[i].startsWith("--")) {
+      res.push(`${commandLineArgs[i]} is ${commandLineArgs[i + 1]}`);
+      i++;
+    }
+  }
+
+  console.log(commandLineArgsWithValues.join(", "));
 };
 
 parseArgs();
